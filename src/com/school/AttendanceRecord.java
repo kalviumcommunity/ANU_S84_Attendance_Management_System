b853fcb9cb7c7734085430f3f562cb587c66a58a
ANU-2524 <anusoni25.2006@gmail.com>
2025-09-15 10:44:14 +0530

    feat: Implement inheritance hierarchy for Person, Student, Teacher, Staff (Part 5)

--- src/com/school/AttendanceRecord.java
+++ src/com/school/AttendanceRecord.java
@@ -2,21 +2,24 @@ package com.school;
 
 public class AttendanceRecord {
     private int studentId;
-    private int courseId;
-    private String status ;
+    private int courseID;
+    private String status;
 
-    public AttendanceRecord(int studentId, int courseId, String status){
-        this.studentId = studentId;
-        this.courseId = courseId;
-        
-        if("Present".equalsIgnoreCase(status) || "Absent".equalsIgnoreCase(status)) {
-            this.status = status;
-        }else{
-            this.status = "Invalid" ;
-            System.out.println("Warning : Invalid attendance status provided. Set to 'Invalid'.");
+    public AttendanceRecord(int studentID, int courseID, String status){
+        this.studentId= studentID;
+        this.courseID= courseID;
+
+        if("Present".equalsIgnoreCase(status) || "Absent".equalsIgnoreCase(status)){
+            this.status= status;
+        }
+        else{
+            this.status= "Invalid";
+            System.out.println("Warning: Invalid attendance status provided. Set to 'Invalid'.");
         }
     }
-    public void displayRecord() {
-        System.out.println("Attendance : Student ID " + studentId + " in Course ID C" + courseId + " - Status: " + status);
+
+    public void displayRecord(){
+        System.out.println("Attendance: Student ID " + studentId + " in Course ID C" + courseID + " - Status: " + status);
     }
+
 }
